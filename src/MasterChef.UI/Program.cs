f62046773505341309125715127db45effa68d8e
Luiz Aguiar <luizmanoel00@gmail.com>
2022-12-18 21:01:52 -0300

    update IoC collection

--- src/MasterChef.UI/Program.cs
+++ src/MasterChef.UI/Program.cs
@@ -24,7 +24,7 @@ builder.Host.UseSerilog(Log.Logger);
 
 Console.Title = Assembly.GetEntryAssembly().GetName().Name;
 
-builder.Services.AddServiceIoCDependency();
+builder.Services.AddUIServiceIoCDependency();
 
 builder.Services.BuildServiceProvider().MigrateIdentityDatabase();
 
