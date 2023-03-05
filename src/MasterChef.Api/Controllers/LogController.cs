bff9e94cce10ddf51ebf0b0ad6239a67002bb236
Luiz Aguiar <luizmanoel00@gmail.com>
2023-03-05 20:53:11 -0300

    XML comments

--- src/MasterChef.Api/Controllers/LogController.cs
+++ src/MasterChef.Api/Controllers/LogController.cs
@@ -5,17 +5,29 @@ using Nest;
 
 namespace MasterChef.Api.Controllers;
 
+/// <summary>
+/// Log Controller
+/// </summary>
 [ApiController]
 [Route("[controller]")]
 public class LogController : ControllerBase
 {
     private readonly IElasticClient _elasticClient;
 
+    /// <summary>
+    /// Log Controller
+    /// </summary>
+    /// <param name="elasticClient"></param>
     public LogController(IElasticClient elasticClient)
     {
         _elasticClient = elasticClient;
     }
 
+    /// <summary>
+    /// Get All Logs
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="ElasticsearchClientException"></exception>
     [HttpGet("all")]
     public async Task<IActionResult> GetAllLogs()
     {
@@ -32,6 +44,13 @@ public class LogController : ControllerBase
         return Ok(response.Documents.ToList());
     }
 
+    /// <summary>
+    /// Get Logs by Date
+    /// </summary>
+    /// <param name="dateBegin"></param>
+    /// <param name="dateEnd"></param>
+    /// <returns></returns>
+    /// <exception cref="ElasticsearchClientException"></exception>
     [HttpGet("date")]
     public async Task<IActionResult> GetLogsByDate([FromQuery] DateTime dateBegin, [FromQuery] DateTime dateEnd)
     {
