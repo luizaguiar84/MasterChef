1c73059baaa7091fcde38807414c1c7d9745a7f3
Luiz Aguiar <luizmanoel00@gmail.com>
2023-07-08 18:48:14 -0300

    save recipe test

--- Test.Application/RecipeAppServiceTests.cs
+++ Test.Application/RecipeAppServiceTests.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using FluentValidation;
+using FluentValidation.Results;
 using MasterChef.Application.Interfaces;
 using MasterChef.Application.Services;
 using MasterChef.Domain.Entities;
@@ -23,13 +24,13 @@ public class RecipeAppServiceTests
     private readonly Mock<IValidator<RecipeDto>> _validation = new();
     private readonly Mock<IEventService> _eventService = new();
     private readonly Mock<IRecipeRepository> _recipeRepository = new();
-        
+
     public RecipeAppServiceTests()
     {
         var myProfile = new MappingProfiles();
         var configuration = new MapperConfiguration(c => c.AddProfile(myProfile));
         _mapper = new Mapper(configuration);
-        
+
         _service = new RecipeAppService(
             _validation.Object,
             _eventService.Object,
@@ -40,36 +41,37 @@ public class RecipeAppServiceTests
             _cache.Object
         );
     }
-    
+
     [Fact]
     public async Task GetRecipeById()
     {
         var id = 1;
-        
-        var recipeResponse = new Recipe(){Id = id};
+
+        var recipeResponse = new Recipe() { Id = id };
 
         _recipeRepository.Setup(r => r.GetByIdAsync(id))
             .ReturnsAsync(recipeResponse);
 
         var recipe = await _service.GetById(id);
-        
+
         Assert.NotNull(recipe);
     }
+
     [Fact]
     public async Task GetRecipeByIdWithInvalidId()
     {
         var id = 1;
-        
-        var recipeResponse = new Recipe(){Id = id};
+
+        var recipeResponse = new Recipe() { Id = id };
 
         _recipeRepository.Setup(r => r.GetByIdAsync(id))
             .ReturnsAsync(recipeResponse);
 
         var recipe = await _service.GetById(id + 1);
-        
+
         Assert.Null(recipe);
     }
-    
+
     [Fact]
     public async Task InactivateRecipes()
     {
@@ -83,10 +85,47 @@ public class RecipeAppServiceTests
 
         _recipeRepository.Setup(r => r.GetByIdAsync(id))
             .ReturnsAsync(activeRecipe);
-            
+
         var recipe = await _service.Inactivate(id);
-        
+
         Assert.NotNull(recipe);
         Assert.False(recipe.Active);
+    }
+
+    [Fact]
+    public async Task SaveRecipe()
+    {
+        var id = 1;
+
+        var dto = new RecipeDto()
+        {
+            User = new UserDto()
+            {
+                ExternalId = "externalId"
+            }
+        };
+
+        var responseRecipe = new Recipe()
+        {
+            Active = true,
+            Id = id
+        };
+        var user = new User() { Id = 1 };
+
+        _validation.Setup(v => v.ValidateAsync(dto, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new ValidationResult());
+
+        _recipeRepository.Setup(r => r.GetByIdAsync(id))
+            .ReturnsAsync(responseRecipe);
+
+        _userRepository.Setup(u => u.GetByExternalId(dto.User.ExternalId))
+            .ReturnsAsync(user);
+
+        _recipeRepository.Setup(r => r.AddAsync(It.IsAny<Recipe>()))
+            .ReturnsAsync(responseRecipe);
+
+        var recipe = await _service.Save(dto);
+
+        Assert.NotNull(recipe);
     }
 }
